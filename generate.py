cbd4810fb92cf6fc7489f609b826e82e82778232
AIMEPTSTSAM <aimeptstsam@mail.com>
2026-02-27 16:56:28 +0100

    update

--- generate.py
+++ generate.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     user_key = input("Enter your user key: ")
     site_key = input("Enter your site key: ")
 
-    bytes = derive_password(master_key, user_key, site_key)
-    print(bytes)
-    print(sanitize_password(bytes))
+    entropy = derive_password(master_key, user_key, site_key)
+    print(sanitize_password(entropy))
 
